b90df8bba320859e9a1fce73b011c135897bd8ef
nandstech01 <contact@nands.tech>
2025-11-28 15:01:08 +0900

    Fix: Enable scrolling on dm-nands page

--- app/dm-nands/layout.tsx
+++ app/dm-nands/layout.tsx
@@ -24,7 +24,7 @@ export default function DmNandsLayout({
             display: none !important;
           }
           body {
-            overflow: hidden !important;
+            overflow: auto !important;
           }
         `
       }} />
